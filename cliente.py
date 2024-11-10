e7506121df4edc8c3903c63224a35a644f119088
Gustavoyfi <163774342+Gustavoyfi@users.noreply.github.com>
2024-11-10 16:19:13 -0300

    update

--- cliente.py
+++ cliente.py
@@ -1,25 +1,142 @@
-import socket as sock
+import threading
+import socket
+import argparse
+import sys
+import tkinter as tk
 
-HOST = "127.0.0.1" #IP DO SERVIDOR
-PORTA = 9999 #PORTA DO SERVIDOR
+class Send(threading.Thread):
+    def __init__(self, sock, name, running_flag):
+        super().__init__()
+        self.sock = sock
+        self.name = name
+        self.running_flag = running_flag  
+        
+    def run(self):
+        while self.running_flag.is_set():
+            message = sys.stdin.readline()[:-1]
+            if message == "QUIT":
+                self.sock.sendall(f"Server: {self.name} saiu do chat".encode("ascii"))
+                print("\nSaindo...")
+                self.sock.close()
+                self.running_flag.clear()
+            else:
+                self.sock.sendall(message.encode("ascii"))
 
-# CRIAR SOCKET IPV4/TCP
-s = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
+class Receive(threading.Thread):
+    def __init__(self, sock, name, messages, running_flag):
+        super().__init__()
+        self.sock = sock
+        self.name = name
+        self.messages = messages
+        self.running_flag = running_flag
+        
+    def run(self):
+        while self.running_flag.is_set():
+            try:
+                message = self.sock.recv(1024).decode("ascii")
+                if message:
+                    self.messages.insert(tk.END, message)
+                    print(f"\r{message}\n{self.name}: ", end=" ")
+                else:
+                    print("\nPerda de conexão com o servidor!")
+                    self.running_flag.clear()
+                    self.sock.close()
+            except OSError:
+                break
 
-#CLIENTE 
-s.conncet((HOST, PORTA))
+class Client:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.name = None
+        self.messages = None
+        self.running_flag = threading.Event()
+        self.running_flag.set()
 
-#CLIENTE ENVIA UMA MENSAGEM
-# s.sendall(str.encode("BES - PUCPR - O MELHOR CURSO DO BRASIL"))
-#Abram um input e envie a mensagem para o servidor
+    def start(self):
+        print(f"Tentando conectar-se a {self.host}:{self.port}...")
+        self.sock.connect((self.host, self.port))
+        print(f"Conexão bem-sucedida com {self.host}:{self.port}")
 
-#Cliente le uma mensagem
-dados = s.recv(1024)
-print(f"Servidor >> {dados.decode()}")
+        
+        while True:
+            self.name = input("Escolha seu nome: ").strip()
+            self.sock.sendall(self.name.encode("ascii"))
+            response = self.sock.recv(1024).decode("ascii")
+            if response == "Nome de usuário já em uso. Por favor, escolha outro.":
+                print(response)
+            else:
+                print(f"Bem-vindo(a), {self.name}! Preparado(a) para enviar e receber mensagens...")
+                break
 
-nome = input("Informe seu nome: ")
+        send = Send(self.sock, self.name, self.running_flag)
+        receive = Receive(self.sock, self.name, self.messages, self.running_flag)
+        send.start()
+        receive.start()
 
-while True:
-    mensagem = input(" ")
-    s.sendall(str.encode(f"{nome} >> {mensagem}"))
+        print("Pronto! Saia do chat a qualquer momento digitando 'QUIT'\n")
+        print(f"{self.name}: ", end="")
 
+        return receive
+
+    def send(self, textInput):
+        message = textInput.get()
+        textInput.delete(0, tk.END)
+        self.messages.insert(tk.END, f"{self.name}: {message}")
+
+        self.sock.sendall(message.encode("ascii"))
+
+def main():
+    parser = argparse.ArgumentParser(description="Chat Client")
+    parser.add_argument("host", type=str, help="IP ou nome do servidor")
+    parser.add_argument("port", type=int, help="Porta do servidor")
+    args = parser.parse_args()
+
+    client = Client(args.host, args.port)
+    receive = client.start()
+    
+    window = tk.Tk()
+    window.title("Chatroom Monstrão")
+    
+    fromMessage = tk.Frame(master=window)
+    scrollBar = tk.Scrollbar(master=fromMessage)
+    messages = tk.Listbox(master=fromMessage, yscrollcommand=scrollBar.set)
+    scrollBar.pack(side=tk.RIGHT, fill=tk.Y, expand=False)
+    messages.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+    
+    client.messages = messages
+    receive.messages = messages
+    
+    fromMessage.grid(row=0, column=0, columnspan=2, sticky="nsew")
+    fromEntry = tk.Frame(master=window)
+    textInput = tk.Entry(master=fromEntry)
+    
+    textInput.pack(fill=tk.BOTH, expand=True)
+    textInput.bind("<Return>", lambda x: client.send(textInput))
+    textInput.insert(0, "Escreva sua mensagem aqui.")
+    
+    btnSend = tk.Button(
+        master=window,
+        text="Enviar",
+        command=lambda: client.send(textInput)
+    )
+    
+    fromEntry.grid(row=1, column=0, padx=10, sticky="ew")
+    btnSend.grid(row=1, column=1, padx=10, sticky="ew")
+    
+    window.rowconfigure(0, minsize=500, weight=1)
+    window.rowconfigure(1, minsize=50, weight=0)
+    window.columnconfigure(0, minsize=500, weight=1)
+    window.columnconfigure(1, minsize=200, weight=0)
+
+    def on_closing():
+        client.running_flag.clear()
+        client.sock.close()
+        window.destroy()
+
+    window.protocol("WM_DELETE_WINDOW", on_closing)
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
--- servidor.py
+++ servidor.py
@@ -1,35 +1,122 @@
-import socket as sock
+import threading
+import socket
+import argparse
+import sys
 
-HOST = "localhost" #127.0.0.1
-PORTA = 9999
+class Server(threading.Thread):
+    
+    def __init__(self, host, port):
+        super().__init__()
+        self.connections = {}
+        self.host = host
+        self.port = port
+        self.running = True  
+        
+    def run(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((self.host, self.port))
+        
+        self.sock.listen(5)
+        print("Ouvindo no", self.sock.getsockname())
+      
+        while self.running:
+            try:
+                sc, sockname = self.sock.accept()
+                print(f"Aceitando uma nova conexão de {sc.getpeername()} para {sc.getsockname()}")
+                
+                username = sc.recv(1024).decode("ascii")
+                
+                
+                if username in self.connections:
+                    sc.sendall("Nome de usuário já em uso. Por favor, escolha outro.".encode("ascii"))
+                    sc.close()
+                    continue
 
-#IPV4 (primeiro parametro): AF_INET
-#TCP (segundo parametro): SOCK_STREAM
-#CRIAR O SOCKET DO SERVIDOR
+                self.connections[username] = sc
+                print(f"{username} entrou no chat.")
+                
+              
+                self.broadcast(f"Bem-vindo(a), {username} entrou no chat!", "Servidor")
+                
+                server_socket = ServerSocket(sc, username, self)
+                server_socket.start()
+            except OSError:
+                break  
+        
+    def broadcast(self, message, source):
+        for username, connection in self.connections.items():
+            if username != source:
+                connection.sendall(message.encode("ascii"))
+    
+    def send_private(self, message, recipient, sender):
+        if recipient in self.connections:
+            self.connections[recipient].sendall(f"[Privado de {sender}]: {message}".encode("ascii"))
+        else:
+            print(f"Usuário {recipient} não encontrado.")
+    
+    def remove_connection(self, username):
+        if username in self.connections:
+            del self.connections[username]
+           
+            self.broadcast(f"{username} saiu do chat.", "Servidor")
 
-s = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
+    def stop(self):
+        self.running = False
+        for connection in self.connections.values():
+            connection.close()
+        self.sock.close()  
 
-#FAZEMOS O "PLUG" DO IP DO SERVIDOR COM A PORTA (BIND)
-s.bind((HOST, PORTA))
+class ServerSocket(threading.Thread):
+    
+    def __init__(self, sc, username, server):
+        super().__init__()
+        self.sc = sc
+        self.username = username
+        self.server = server
+        
+    def run(self):
+        while True:
+            try:
+                message = self.sc.recv(1024).decode("ascii")
+                if message:
+                    if message.startswith("/private"):  
+                        _, recipient, *msg = message.split()
+                        msg = " ".join(msg)
+                        self.server.send_private(msg, recipient, self.username)
+                    else:  
+                        public_message = f"{self.username}: {message}"
+                        self.server.broadcast(public_message, self.username)
+                else: 
+                    print(f"{self.username} fechou a conexão")
+                    self.sc.close()
+                    self.server.remove_connection(self.username)
+                    return
+            except Exception as e:
+                print(f"Erro: {e}")
+                break
+            
 
-#COLOCAMOS O SERVIDOR NO MODO DE ESCUTA (LISTEN) - AGUARDANDO CONEXÕES
-s.listen()
-print(f"O servidor {HOST}:{PORTA} está aguardando conexões...")
-
-#SERVIDOR PRECISA ACEITAR CONEXÃO
-
-while True:
-
-    conn, ender = s.accept()
-    print(f"Conexão estabelecida com {ender}")
-
+def exit(server):
     while True:
-        
-        #Recebimento de mensagem
-        dados = conn.recv(1024)
-        # print(f"Cliente - {ender} >> {dados.decode()}")
-        print(dados.decode())
-        
-        #Envio de mensagem (bytes)
-        # conn.sendall(dados)
+        ipt = input("")
+        if ipt == "q":
+            print("Fechando todas conexões...")
+            server.stop()  
+            print("Desligando o servidor....")
+            server.join()  
+            break  
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Chatroom Server")
+    parser.add_argument("host", help="Interface the server listens at")
+    parser.add_argument("-p", metavar="PORT", type=int, default=1060, help="TCP port (default 1060)")
+    
+    args = parser.parse_args()
+    
+    server = Server(args.host, args.p)
+    server.start()
+    
+    exit_thread = threading.Thread(target=exit, args=(server,))
+    exit_thread.start()
+    exit_thread.join()
